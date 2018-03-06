fb2f32ee320a3aac2e204c513014d63ae2e0a03e
PhilippineG <philippinegilabert@gmail.com>
2018-03-06 16:08:10 +0100

    correction

--- Script MySQL.sql
+++ Script MySQL.sql
@@ -4,10 +4,10 @@
 
 
 #------------------------------------------------------------
-# Table: stations
+# Table: Stations
 #------------------------------------------------------------
 
-CREATE TABLE stations(
+CREATE TABLE Stations(
         id_station  int (11) Auto_increment  NOT NULL ,
         nom_station Varchar (100) NOT NULL ,
         id_commune  Int NOT NULL ,
@@ -16,10 +16,10 @@ CREATE TABLE stations(
 
 
 #------------------------------------------------------------
-# Table: communes
+# Table: Communes
 #------------------------------------------------------------
 
-CREATE TABLE communes(
+CREATE TABLE Communes(
         id_commune  int (11) Auto_increment  NOT NULL ,
         nom_commune Varchar (100) NOT NULL ,
         PRIMARY KEY (id_commune )
@@ -27,10 +27,10 @@ CREATE TABLE communes(
 
 
 #------------------------------------------------------------
-# Table: constantes relevees
+# Table: Constantes relevees
 #------------------------------------------------------------
 
-CREATE TABLE constantes_relevees(
+CREATE TABLE Constantes_relevees(
         id_const_rel int (11) Auto_increment  NOT NULL ,
         vit_vent     Numeric NOT NULL ,
         temp         Numeric NOT NULL ,
@@ -41,10 +41,10 @@ CREATE TABLE constantes_relevees(
 
 
 #------------------------------------------------------------
-# Table: date
+# Table: Date
 #------------------------------------------------------------
 
-CREATE TABLE date(
+CREATE TABLE Date(
         id_date   int (11) Auto_increment  NOT NULL ,
         date_jour Date NOT NULL ,
         heure     Datetime NOT NULL ,
@@ -53,28 +53,17 @@ CREATE TABLE date(
 
 
 #------------------------------------------------------------
-# Table: sta_con
+# Table: sta_con_dat
 #------------------------------------------------------------
 
-CREATE TABLE sta_con(
+CREATE TABLE sta_con_dat(
         id_station   Int NOT NULL ,
         id_const_rel Int NOT NULL ,
-        PRIMARY KEY (id_station ,id_const_rel )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: con_dat
-#------------------------------------------------------------
-
-CREATE TABLE con_dat(
-        id_const_rel Int NOT NULL ,
         id_date      Int NOT NULL ,
-        PRIMARY KEY (id_const_rel ,id_date )
+        PRIMARY KEY (id_station ,id_const_rel ,id_date )
 )ENGINE=InnoDB;
 
-ALTER TABLE stations ADD CONSTRAINT FK_stations_id_commune FOREIGN KEY (id_commune) REFERENCES communes(id_commune);
-ALTER TABLE sta_con ADD CONSTRAINT FK_sta_con_id_station FOREIGN KEY (id_station) REFERENCES stations(id_station);
-ALTER TABLE sta_con ADD CONSTRAINT FK_sta_con_id_const_rel FOREIGN KEY (id_const_rel) REFERENCES constantes_relevees(id_const_rel);
-ALTER TABLE con_dat ADD CONSTRAINT FK_con_dat_id_const_rel FOREIGN KEY (id_const_rel) REFERENCES constantes_relevees(id_const_rel);
-ALTER TABLE con_dat ADD CONSTRAINT FK_con_dat_id_date FOREIGN KEY (id_date) REFERENCES date(id_date);
+ALTER TABLE Stations ADD CONSTRAINT FK_Stations_id_commune FOREIGN KEY (id_commune) REFERENCES Communes(id_commune);
+ALTER TABLE sta_con_dat ADD CONSTRAINT FK_sta_con_dat_id_station FOREIGN KEY (id_station) REFERENCES Stations(id_station);
+ALTER TABLE sta_con_dat ADD CONSTRAINT FK_sta_con_dat_id_const_rel FOREIGN KEY (id_const_rel) REFERENCES Constantes_relevees(id_const_rel);
+ALTER TABLE sta_con_dat ADD CONSTRAINT FK_sta_con_dat_id_date FOREIGN KEY (id_date) REFERENCES Date(id_date);
